e2afbcebdfe14985c5a1d0168ef00d9fbbb61902
Trace Ohrt <teohrt18@gmail.com>
2025-12-04 13:36:09 -0500

    skeleton

---
+++ Day_1/1.2/go/pkg/solution/solution.go
@@ -0,0 +1,42 @@
+package solution
+
+import (
+	"log"
+	"solution/pkg/lineIterator"
+	"strconv"
+)
+
+func Solve(filePath string) int {
+	iterator, err := lineIterator.NewLineIterator(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer iterator.Close()
+
+	result := 0
+	dialPosition := 50
+
+	for iterator.Next() {
+		line := iterator.Line()
+		direction := line[0]
+		distance, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch direction {
+		case 'L':
+			dialPosition -= distance
+		case 'R':
+			dialPosition += distance
+		default:
+			log.Fatalf("invalid direction: %s", string(direction))
+		}
+
+		dialPosition = dialPosition % 100
+		if dialPosition == 0 {
+			result++
+		}
+	}
+
+	return result
+}
