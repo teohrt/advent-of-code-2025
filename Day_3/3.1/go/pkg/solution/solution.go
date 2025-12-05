3f4de9d3d5b678485ef16a7036fc062df9abe665
Trace Ohrt <teohrt18@gmail.com>
2025-12-04 23:00:24 -0500

    correct answer

--- Day_3/3.1/go/pkg/solution/solution.go
+++ Day_3/3.1/go/pkg/solution/solution.go
@@ -1,9 +1,9 @@
 package solution
 
 import (
-	"fmt"
 	"log"
 	"solution/pkg/lineIterator"
+	"strconv"
 )
 
 func Solve(filePath string) int {
@@ -16,8 +16,39 @@ func Solve(filePath string) int {
 	result := 0
 
 	for iterator.Next() {
+		largestLeft := 0
+		leftIndex := 0
+		largestRight := 0
 		line := iterator.Line()
-		fmt.Println(line)
+
+		// find largest left digit
+		// exclude the last character - the  last character can only be the right digit
+		for i := 0; i < len(line)-1; i++ {
+			digit := line[i]
+			digitInt, err := strconv.Atoi(string(digit))
+			if err != nil {
+				log.Fatal(err)
+			}
+			if digitInt > largestLeft {
+				largestLeft = digitInt
+				leftIndex = i
+			}
+		}
+
+		// find largest right digit
+		for i := leftIndex + 1; i < len(line); i++ {
+			digit := line[i]
+			digitInt, err := strconv.Atoi(string(digit))
+			if err != nil {
+				log.Fatal(err)
+			}
+			if digitInt > largestRight {
+				largestRight = digitInt
+			}
+		}
+
+		// avoids string conversions
+		result += (largestLeft * 10) + largestRight
 	}
 
 	return result
