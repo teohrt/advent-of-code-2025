a9c40c70d8b39b0e32f6420b6fe8e287172340df
Trace Ohrt <teohrt18@gmail.com>
2025-12-06 18:33:33 -0500

    correct answer for 6.1

--- Day_6/6.1/go/pkg/solution/solution.go
+++ Day_6/6.1/go/pkg/solution/solution.go
@@ -3,7 +3,6 @@ package solution
 import (
 	"log"
 	"solution/pkg/lineIterator"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,76 +14,42 @@ func Solve(filePath string) int {
 	}
 	defer iterator.Close()
 
-	initialIntervals := [][]int{}
-	// parse the initial intervals
+	var grid [][]string
 	for iterator.Next() {
 		line := iterator.Line()
-		if line == "" {
-			break // end of intervals, continuuing would enter into the available ingredient id portion of the input
-		}
-		intervals := strings.Split(line, "-")
-		if len(intervals) != 2 {
-			log.Fatalf("invalid line: %s", line)
-		}
-		start, err := strconv.Atoi(intervals[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		end, err := strconv.Atoi(intervals[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		initialIntervals = append(initialIntervals, []int{start, end})
+		elements := strings.Fields(line)
+		grid = append(grid, elements)
 	}
 
-	// sort and merge the intervals
-	mergedIntervals := mergeOverlappingIntervals(initialIntervals)
+	totals := []int{}
+
+	for x := 0; x < len(grid[0]); x++ {
+		operator := grid[len(grid)-1][x] // the operator is the last row of the grid
+
+		var total int
+		if operator == "+" {
+			total = 0
+		} else {
+			total = 1
+		}
+
+		for y := 0; y < len(grid)-1; y++ {
+			number, err := strconv.Atoi(grid[y][x])
+			if err != nil {
+				log.Fatal(err)
+			}
+			if operator == "+" {
+				total += number
+			} else {
+				total *= number
+			}
+		}
+		totals = append(totals, total)
+	}
 
 	result := 0
-	// parse the ids and binary search to see if they are in any of the merged intervals
-	for iterator.Next() {
-		line := iterator.Line()
-		id, err := strconv.Atoi(line)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if isInInterval(mergedIntervals, id) {
-			result++
-		}
+	for _, total := range totals {
+		result += total
 	}
-
 	return result
-}
-
-func mergeOverlappingIntervals(intervals [][]int) [][]int {
-	sort.Slice(intervals, func(i, j int) bool {
-		return intervals[i][0] < intervals[j][0]
-	})
-	mergedIntervals := [][]int{}
-	for _, interval := range intervals {
-		if len(mergedIntervals) == 0 || mergedIntervals[len(mergedIntervals)-1][1] < interval[0] {
-			mergedIntervals = append(mergedIntervals, interval)
-		} else {
-			mergedIntervals[len(mergedIntervals)-1][1] = max(mergedIntervals[len(mergedIntervals)-1][1], interval[1])
-		}
-	}
-	return mergedIntervals
-}
-
-func isInInterval(intervals [][]int, id int) bool {
-	// binary search for the interval that contains the id
-	low := 0
-	high := len(intervals) - 1
-	for low <= high {
-		mid := (low + high) / 2
-		if intervals[mid][0] <= id && intervals[mid][1] >= id {
-			return true
-		}
-		if intervals[mid][0] > id {
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
-	}
-	return false
 }
