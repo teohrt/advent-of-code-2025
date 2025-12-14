f367d2fe788cdcab79c84cd6c1033d532da43507
Trace Ohrt <teohrt18@gmail.com>
2025-12-14 14:37:37 -0600

    working distance formula

--- Day_8/8.1/go/pkg/solution/solution.go
+++ Day_8/8.1/go/pkg/solution/solution.go
@@ -3,6 +3,7 @@ package solution
 import (
 	"fmt"
 	"log"
+	"math"
 	"solution/pkg/lineIterator"
 	"strconv"
 	"strings"
@@ -46,9 +47,17 @@ func Solve(filePath string) int {
 	}
 
 	result := 0
-	for _, coord := range coords {
-		fmt.Printf("coord: %+v\n", coord)
-	}
+
+	c1 := coords[0]
+	c2 := coords[1]
+	fmt.Printf("c1: %+v\n", c1)
+	fmt.Printf("c2: %+v\n", c2)
+	fmt.Printf("distance: %f\n", getDistance(c1, c2))
 
 	return result
+}
+
+// d = sqrt((x2-x1)^2 + (y2-y1)^2 + (z2-z1)^2)
+func getDistance(coord1 Coordinate, coord2 Coordinate) float64 {
+	return math.Sqrt(float64(math.Pow(float64(coord2.x-coord1.x), 2) + math.Pow(float64(coord2.y-coord1.y), 2) + math.Pow(float64(coord2.z-coord1.z), 2)))
 }
