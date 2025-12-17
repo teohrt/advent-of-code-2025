b129308889d96e04b9edf3f865aaa7cf4438e810
Trace Ohrt <teohrt18@gmail.com>
2025-12-17 17:16:45 -0600

    correct answer for 10.1

--- Day_10/10.1/go/pkg/solution/solution.go
+++ Day_10/10.1/go/pkg/solution/solution.go
@@ -2,7 +2,6 @@ package solution
 
 import (
 	"fmt"
-	"math"
 )
 
 /*
@@ -14,45 +13,10 @@ result_total = 0
 
 for every machine:
 	buttonMap = create button map
-	iterative DFS over button map
+	iterative BFS over button states
 		update result_total
 
 return result_total
-
-
-Deeper dive:
---------------------------------
-
-Example machine:
-[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-
-map:
-0: (0, 2), (0, 1)
-1: (1, 3), (0, 1)
-2: (2), (2, 3), (0, 2)
-3: (3), (1,3), (2, 3)
-
-iterative DFS:
-
-state = [0,0,0,0]
-desires = [1, 2]
-pressCount = 0
-
-min_count = infinity
-stack = [(state, desires, pressCount)]
-while stack
-	state, desires, pressCount = stack.pop()
-	if pressCount > min_count:
-		continue
-	if desires are satisfied:
-		update min_count
-		continue
-	else:
-		for every desire:
-			for every associated button:
-				push new state, desires & pressCount onto the stack
-
-return min_count
 */
 
 func Solve(filePath string) int {
@@ -60,84 +24,83 @@ func Solve(filePath string) int {
 
 	result := 0
 	for i, machine := range machines {
-		result += getMinPresses(machine)
-		fmt.Printf("machine %d, min presses: %d\n", i, getMinPresses(machine))
+		minPresses := getMinPresses(machine)
+		result += minPresses
+		fmt.Printf("machine %d, min presses: %d\n", i, minPresses)
 	}
 
 	return result
 }
 
 type Item struct {
-	lightStates     []bool
-	previousButtons map[string]struct{}
+	lightStates []bool
+	presses     int
 }
 
+// BFS to find the minimum number of presses to achieve the desired state
 func getMinPresses(machine Machine) int {
-	buttonMap := make(map[int][][]int)
-	for _, b := range machine.buttons {
-		for _, button := range b {
-			buttonMap[button] = append(buttonMap[button], b)
+	targetState := machine.desiredLights
+	numLights := len(targetState)
+
+	// Helper to convert []bool to string for map key
+	stateToString := func(states []bool) string {
+		b := make([]byte, numLights)
+		for i, s := range states {
+			if s {
+				b[i] = '1'
+			} else {
+				b[i] = '0'
+			}
 		}
+		return string(b)
 	}
 
-	minPresses := int(float64(math.Inf(1)))
-	stack := []Item{
+	initialStates := make([]bool, numLights)
+	initialStateStr := stateToString(initialStates)
+	targetStateStr := stateToString(targetState)
+
+	if initialStateStr == targetStateStr {
+		return 0
+	}
+
+	queue := []Item{
 		{
-			lightStates:     make([]bool, len(machine.desiredLights)), // initially all lights are off
-			previousButtons: make(map[string]struct{}),
+			lightStates: initialStates,
+			presses:     0,
 		},
 	}
-	for len(stack) > 0 {
-		var popped Item
-		popped, stack = stack[0], stack[1:]
 
-		// if the number of presses is greater than the minimum, skip further processing and continue
-		// this is a pruning optimization - if we've already found a better solution, we don't need to continue
-		if len(popped.previousButtons) >= minPresses {
-			continue
-		}
+	visited := make(map[string]bool)
+	visited[initialStateStr] = true
 
-		// determine the lights that need to be flipped to achieve the desired state
-		// if none, we've found the desired state, can update the minimum and continue
-		lightsToFlip := []int{}
-		for idx, desiredLightValue := range machine.desiredLights {
-			if desiredLightValue != popped.lightStates[idx] {
-				lightsToFlip = append(lightsToFlip, idx)
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		for _, button := range machine.buttons {
+			// Create new state by toggling lights listed in the button
+			nextStates := make([]bool, numLights)
+			copy(nextStates, current.lightStates)
+			for _, lightIdx := range button {
+				nextStates[lightIdx] = !nextStates[lightIdx]
 			}
-		}
-		foundDesiredState := len(lightsToFlip) == 0
-		if foundDesiredState {
-			minPresses = len(popped.previousButtons)
-			continue
-		}
 
-		// push new states onto the stack
-		for _, idxToFlip := range lightsToFlip {
-			for _, button := range buttonMap[idxToFlip] {
-				// If we've already pressed this button, skip
-				buttonString := fmt.Sprintf("%d", button)
-				if _, ok := popped.previousButtons[buttonString]; ok {
-					continue
-				}
-				// "Press the button" & update the light states associated
-				newLightStates := make([]bool, len(popped.lightStates))
-				copy(newLightStates, popped.lightStates)
-				for _, lightIdx := range button {
-					newLightStates[lightIdx] = !newLightStates[lightIdx]
-				}
-				newPreviousButtons := make(map[string]struct{})
-				for k, v := range popped.previousButtons {
-					newPreviousButtons[k] = v
-				}
-				newPreviousButtons[buttonString] = struct{}{}
-				item := Item{
-					lightStates:     newLightStates,
-					previousButtons: newPreviousButtons,
-				}
-				stack = append(stack, item)
+			nextStateStr := stateToString(nextStates)
+			if nextStateStr == targetStateStr {
+				return current.presses + 1
+			}
+
+			// If we've already visited the next state, it's guarenteed that
+			// we got there with <= presses than whatever we have now, so we can skip
+			if !visited[nextStateStr] {
+				visited[nextStateStr] = true
+				queue = append(queue, Item{
+					lightStates: nextStates,
+					presses:     current.presses + 1,
+				})
 			}
 		}
 	}
 
-	return int(minPresses)
+	return 0 // Should not happen based on problem description
 }
